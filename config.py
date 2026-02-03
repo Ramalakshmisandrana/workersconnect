2ed61ab3cbd8c91d07f1d36de4f9cec269721b52
Mohan Thambabattula <mohanthambabattula@gmail.com>
2026-02-03 23:46:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SESSION_SECRET') or 'workersconnect-secret-key-2024'
 
-    # Render persistent disk path
-    DB_PATH = "/data/workersconnect.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
